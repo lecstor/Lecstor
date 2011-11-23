36c5ddbb4b51a84512a197f00fb9e9ca86f227e9
Jason Galea <jason@lecstor.com>
2011-11-23 12:39:12 +1000

    added some docs

--- lib/Lecstor/DBIxClass.pm
+++ lib/Lecstor/DBIxClass.pm
@@ -2,4 +2,16 @@ use strict;
 use warnings;
 package Lecstor::DBIxClass;
 
+# ABSTRACT: provides classes for simplifying the use of DBIx::Class schemas
+
+=head1 DESCRIPTION
+
+L<Lecstor::DBIxClass::Role::Editor>
+
+A Moose role providing create & update methods which take a basic hashref of data.
+
+=cut
+
+
+
 1;
